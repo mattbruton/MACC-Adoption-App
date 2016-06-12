bd96a7c199bd327b18d199b74c887de224593ef6
MATT BRUTON <the.bruton@gmail.com>
2016-06-11 20:20:02 -0500

    removed code from app.js from trying to work around CORS problem

--- app/app.js
+++ app/app.js
@@ -2,12 +2,6 @@
 
 var app = angular.module('MaccAdopt', ['ngRoute', 'ui.materialize']);
 
-app.config(function($httpProvider) {
-      //Enable cross domain calls
-      $httpProvider.defaults.useXDomain = true;
- 
-      //Remove the header used to identify ajax call  that would prevent CORS from working
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  });
+
   
 console.log("test");
