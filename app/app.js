64ce6b34fd7d0d99400d4a2e153582be60dd1f9f
MATT BRUTON <the.bruton@gmail.com>
2016-06-18 07:36:17 -0500

    made adjustments to carousel, not super thrilled with any of the options i've found. will create one

--- app/app.js
+++ app/app.js
@@ -1,11 +1,20 @@
 "use strict";
 
-var app = angular.module('MaccAdopt', ['ngRoute', 'ui.materialize'])
+var app = angular.module('MaccAdopt', ['ngRoute', 'ui.materialize', 'slickCarousel'])
   .constant("firebaseURL", `https://nss-matt-fe-capstone.firebaseio.com/`);
+
+app.config(['slickCarouselConfig', function (slickCarouselConfig) {
+    slickCarouselConfig.dots = true;
+    slickCarouselConfig.autoplay = false;
+  }])
 
 app.config(function($routeProvider){
   $routeProvider.
     when('/',{
+      templateUrl: 'partials/carousel.html',
+      controller: 'CarouselCtrl'
+      }).
+    when('/search',{
       templateUrl: 'partials/search.html',
       controller: 'SearchCtrl'
       }).
