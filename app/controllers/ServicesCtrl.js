df723c20dc5eb560edc0b9adb9dfe231b5cb20c0
MATT BRUTON <the.bruton@gmail.com>
2016-06-25 23:38:47 -0500

    cleaned up html files

--- app/controllers/ServicesCtrl.js
+++ app/controllers/ServicesCtrl.js
@@ -22,6 +22,13 @@ app.controller("ServicesCtrl", function($scope) {
         content4: 'Thursday - 10am-6pm',
         content5: 'Friday - 10am-4pm',
         content6: 'Saturday - 10am-4pm'
+    },
+    {
+        title: '2016 Low-Cost Rabies Clinic Information',
+        text: `The Metro Public Health Department is making a change to the annual rabies vaccination clinics. Metro Public Health Department will host a low-cost rabies vaccination clinic once a month at Metro Animal Care and Control from February-November in 2016.`,
+        text2: 'The monthly rabies clinics will replace the annual rabies clinics previously held each March at area Metro Schools and Metro Parks.',
+        text3: 'The change to monthly vaccination clinics will allow community members more flexibility to obtain low-cost rabies vaccinations throughout the year.',
+        text4: 'MACC recently made a change to the annual low-cost rabies vaccination clinics and now hosts a monthly rabies vaccination clinic the third Saturday of each month at the shelter from February-November in 2016. MACC is pleased to announce additional local veterinarians will be hosting low-cost rabies clinics. Cost: $10.'
     }
 ];
 
