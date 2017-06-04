a71d7e7f3cd4bb8428c108c7b5e7f6ed83b26975
Matt Bruton <the.bruton@gmail.com>
2017-06-04 18:51:59 -0500

    removed unused controllers/factories and references to them in app.js

--- app/app.js
+++ app/app.js
@@ -2,29 +2,11 @@
 
 var app = angular.module('MaccAdopt', ['ngRoute', 'ngCookies']);
 
-// let isAuth = (AuthFactory) => new Promise((resolve, reject) => {
-//   if (AuthFactory.isAuthenticated()) {
-//     console.log("User is authenticated, resolve route promise");
-//     resolve();
-//   } else {
-//     console.log("User is not authenticated, reject route promise");
-//     reject();
-//   }
-// });
-
-// app.config(function(MandrillProvider) {
-//   MandrillProvider.setApiKey();
-// });
-
 app.config(function($routeProvider, $locationProvider) {
   $routeProvider.
   when('/', {
     templateUrl: 'partials/main.html'
   }).
-  // when('/details/:id', {
-  //   templateUrl: 'partials/details.html',
-  //   controller: 'FavCtrl'
-  // }).
   when('/services', {
     templateUrl: 'partials/info/info-services.html',
     controller: 'ServicesCtrl'
@@ -33,28 +15,7 @@ app.config(function($routeProvider, $locationProvider) {
     templateUrl: 'partials/search/search-form.html',
     controller: 'SearchCtrl'
   }).
-  // when('/favorites', {
-  //   templateUrl: 'partials/favorites.html',
-  //   controller: 'FavCtrl'
-  // }).
-  // when('/login', {
-  //   templateUrl: 'partials/login.html',
-  //   controller: 'LoginCtrl'
-  // }).
-  // when('/logout', {
-  //   templateUrl: 'partials/login.html',
-  //   controller: 'LoginCtrl'
-  // }).
   otherwise('/');
   
   $locationProvider.html5Mode(true);
 });
-
-// app.run(($location, FirebaseCredentials) => {
-// 	let authConfig = {
-// 		apiKey: FirebaseCredentials.apiKey,
-// 		authDomain: FirebaseCredentials.authDomain,
-// 		databaseURL: FirebaseCredentials.databaseURL
-// 	};
-// 	firebase.initializeApp(authConfig);
-// });
