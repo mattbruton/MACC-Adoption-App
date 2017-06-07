c653810dac7413ad7093d5a841ee5f83b0a90303
Matt Bruton <the.bruton@gmail.com>
2017-06-06 20:27:56 -0500

    implemented style changes for searching and minor layout tweaks, need to finish smaller device view for search results

--- app/app.js
+++ app/app.js
@@ -5,15 +5,16 @@ var app = angular.module('MaccAdopt', ['ngRoute', 'ngCookies']);
 app.config(function($routeProvider, $locationProvider) {
   $routeProvider.
   when('/', {
-    templateUrl: 'partials/main.html'
+    templateUrl: 'partials/search/search-form.html',
+    controller: 'SearchFormCtrl'
+  }).
+  when('/search/results', {
+    templateUrl: 'partials/search/result-list.html',
+    controller: 'SearchResultsCtrl'
   }).
   when('/services', {
     templateUrl: 'partials/info/info-services.html',
     controller: 'ServicesCtrl'
-  }).
-  when('/search', {
-    templateUrl: 'partials/search/search-form.html',
-    controller: 'SearchCtrl'
   }).
   otherwise('/');
   
