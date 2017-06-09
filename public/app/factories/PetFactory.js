afd697c9a033adb76f2dc3ef2a63af81b2b27e11
Matt Bruton <the.bruton@gmail.com>
2017-06-08 22:47:00 -0500

    moved petfinder api call to the node server to resolve cors issues. changed call in PetFactory to account for this change

--- public/app/factories/PetFactory.js
+++ public/app/factories/PetFactory.js
@@ -3,14 +3,9 @@
 app.factory("PetFactory", ['$q', '$http', function ($q, $http) {
 
   const getPetsFromPetfinder = () => {
-    return $http.get("http://api.petfinder.com/shelter.getPets?key=576a9be0464ad49bafe9b98ad8b6eccb&id=TN172&count=200&output=full&format=json")
-      .then((animalCollection) => {
-        let array = [];
-        Object.keys(animalCollection.data.petfinder.pets.pet)
-          .forEach((searchResult) => {
-            array.push(animalCollection.data.petfinder.pets.pet[searchResult]);
-          });
-        return (array);
+    return $http.get("./api/pets")
+      .then((pets) => {
+        return pets.data.pet;
       })
       .catch((error) => {
         console.log(error);
