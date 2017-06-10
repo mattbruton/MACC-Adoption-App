cdd3b90750aea08644eafb5d74a57bc62a01f63a
Matt Bruton <the.bruton@gmail.com>
2017-06-10 17:59:38 -0500

    fixed path to various files in index.html after changing location of node modules

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.get('/api/pets', (req, res, next) => {
   let url = "http://api.petfinder.com/shelter.getPets?key=576a9be0464ad49bafe9b98ad8b6eccb&id=TN172&count=200&output=full&format=json";
